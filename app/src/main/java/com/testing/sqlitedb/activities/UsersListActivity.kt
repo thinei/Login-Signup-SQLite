4ece1ac7d8d6324b3d23c5bdb48b9680ecffe151
barbiekitty.thazin93@gmail.com <barbiekitty.thazin93@gmail.com>
2019-07-12 15:25:00 +0900

    commit project

---
+++ app/src/main/java/com/testing/sqlitedb/activities/UsersListActivity.kt
@@ -0,0 +1,81 @@
+package com.testing.sqlitedb.activities
+
+import android.os.AsyncTask
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.AppCompatTextView
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.testing.sqlitedb.R
+import com.testing.sqlitedb.adapters.UsersRecyclerAdapter
+import com.testing.sqlitedb.model.User
+import com.testing.sqlitedb.sql.DatabaseHelper
+
+
+class UsersListActivity : AppCompatActivity() {
+
+    private val activity = this@UsersListActivity
+    private lateinit var textViewName: AppCompatTextView
+    private lateinit var recyclerViewUsers: RecyclerView
+    private lateinit var listUsers: MutableList<User>
+    private lateinit var usersRecyclerAdapter: UsersRecyclerAdapter
+    private lateinit var databaseHelper: DatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_users_list)
+        getSupportActionBar()!!.hide();
+        //supportActionBar!!.title = ""
+        initViews()
+        initObjects()
+
+    }
+
+    /**
+     * This method is to initialize views
+     */
+    private fun initViews() {
+        textViewName = findViewById<View>(R.id.textViewName) as AppCompatTextView
+        recyclerViewUsers = findViewById<View>(R.id.recyclerViewUsers) as RecyclerView
+    }
+
+    /**
+     * This method is to initialize objects to be used
+     */
+    private fun initObjects() {
+        listUsers = ArrayList()
+        usersRecyclerAdapter = UsersRecyclerAdapter(listUsers)
+
+        val mLayoutManager = LinearLayoutManager(applicationContext)
+        recyclerViewUsers.layoutManager = mLayoutManager
+        recyclerViewUsers.itemAnimator = DefaultItemAnimator()
+        recyclerViewUsers.setHasFixedSize(true)
+        recyclerViewUsers.adapter = usersRecyclerAdapter
+        databaseHelper = DatabaseHelper(activity)
+
+        val emailFromIntent = intent.getStringExtra("EMAIL")
+        textViewName.text = emailFromIntent
+
+        var getDataFromSQLite = GetDataFromSQLite()
+        getDataFromSQLite.execute()
+    }
+
+    /**
+     * This class is to fetch all user records from SQLite
+     */
+    inner class GetDataFromSQLite : AsyncTask<Void, Void, List<User>>() {
+
+        override fun doInBackground(vararg p0: Void?): List<User> {
+            return databaseHelper.getAllUser()
+        }
+
+        override fun onPostExecute(result: List<User>?) {
+            super.onPostExecute(result)
+            listUsers.clear()
+            listUsers.addAll(result!!)
+        }
+
+    }
+}
